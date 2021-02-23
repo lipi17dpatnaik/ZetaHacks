ca4dcd194ba6b049a644a35b0d617b8eca60c186
Lipi Deepaakshi Patnaik <lipi17dpatnaik@gmail.com>
2021-02-23 10:50:12 +0530

    Frontend

---
+++ ZetaHacks/src/components/Tables/OrderedTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <md-table v-model="users" :table-header-color="tableHeaderColor">
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="Stocks">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Returns">{{ item.salary }}</md-table-cell>
+        <md-table-cell md-label="Current">{{ item.country }}</md-table-cell>
+        <md-table-cell md-label="Shares">{{ item.city }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ordered-table",
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      selected: [],
+      users: [
+        {
+          id: 1,
+          name: "Dakota Rice",
+          salary: "$36,738",
+          country: "Niger",
+          city: "Oud-Turnhout"
+        },
+        {
+          id: 2,
+          name: "Minerva Hooper",
+          salary: "$23,738",
+          country: "Curaçao",
+          city: "Sinaai-Waas"
+        },
+        {
+          id: 3,
+          name: "Sage Rodriguez",
+          salary: "$56,142",
+          country: "Netherlands",
+          city: "Overland Park"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ ZetaHacks/src/pages/Layout/DashboardLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <notifications></notifications>
+
+    <side-bar
+      :sidebar-item-color="sidebarBackground"
+      :sidebar-background-image="sidebarBackgroundImage"
+    >
+      <mobile-menu slot="content"></mobile-menu>
+      <sidebar-link to="/dashboard">
+        <md-icon>dashboard</md-icon>
+        <p>Dashboard</p>
+      </sidebar-link>
+      <sidebar-link to="/user">
+        <md-icon>person</md-icon>
+        <p>User Profile</p>
+      </sidebar-link>
+      <sidebar-link to="/typography">
+        <md-icon>verified_user</md-icon>
+        <p>Make Payment</p>
+      </sidebar-link>
+      <sidebar-link to="/table">
+        <md-icon>trending_up</md-icon>
+        <p>Profit Trend</p>
+      </sidebar-link>
+      <!--<sidebar-link to="/icons">
+        <md-icon>bubble_chart</md-icon>
+        <p>Icons</p>
+      </sidebar-link>-->
+      <sidebar-link to="/notifications">
+        <md-icon>notifications</md-icon>
+        <p>Notifications</p>
+      </sidebar-link>
+    </side-bar>
+
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content> </dashboard-content>
+
+      <content-footer v-if="!$route.meta.hideFooter"></content-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "@/pages/Layout/MobileMenu.vue";
+
+export default {
+  components: {
+    TopNavbar,
+    DashboardContent,
+    MobileMenu
+  },
+  data() {
+    return {
+      sidebarBackground: "purple",
+      sidebarBackgroundImage: require("@/assets/img/sidebar-2.jpg")
+    };
+  }
+};
+</script>
